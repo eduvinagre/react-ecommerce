76d3ce885372e4c7732f5ebc4cc0381335d6ed4d
eduvinagre <eduvinagre@gmail.com>
2023-04-26 18:51:29 -0300

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/products:id",
+    element: <Products />,
+  },
+  {
+    path: "/product:id",
+    element: <Product />,
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      Hello world!
-    </div>
-  );
+  return <div>Hello world!</div>;
 }
 
 export default App;
