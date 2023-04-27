79462a11385042537ee234d8f7b41b9fb7f85cd7
eduvinagre <eduvinagre@gmail.com>
2023-04-27 12:40:55 -0300

    initial commit

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="footer"></div>
+    </div>
+  );
 };
 
 export default Footer;
