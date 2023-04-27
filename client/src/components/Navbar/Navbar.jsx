a6a8ba9f5d142bc81ac76fdbcb0b58aa06282ca4
eduvinagre <eduvinagre@gmail.com>
2023-04-27 12:37:49 -0300

    initial commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        <div className="left"></div>
+        <div className="left">
+          <div className="item">
+            <img src="/img/pt.png" alt="icone bandeira do brasil" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>BRL</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link>Women</Link>
+          </div>
+        </div>
         <div className="center"></div>
         <div className="right"></div>
       </div>
