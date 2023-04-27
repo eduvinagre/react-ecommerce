3ba7fb1159f002070633dc1cad6f8a00a2b0b404
eduvinagre <eduvinagre@gmail.com>
2023-04-27 11:17:18 -0300

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,44 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
 import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+  <div className="app">
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </div>;
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/products:id",
-    element: <Products />,
-  },
-  {
-    path: "/product:id",
-    element: <Product />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
   },
 ]);
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
