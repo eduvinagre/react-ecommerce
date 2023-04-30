84b942c3c9f924a0652342dc123a0b2e9938a37e
eduvinagre <eduvinagre@gmail.com>
2023-04-30 09:57:59 -0300

    products first commit

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -4,7 +4,21 @@ import "./Products.scss";
 const Products = () => {
   return (
     <div className="products">
-      <div className="div"></div>
+      <div className="left">
+        <div className="filterItem">
+          <h2>Categorias</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1} />
+            <label htmlFor="1">1</label>
+          </div>
+        </div>
+        <div className="filterItem"></div>
+        <h2>Filtrar por preço</h2>
+        <div className="filterItem">
+          <h2>Classificar</h2>
+        </div>
+      </div>
+      <div className="right"></div>
     </div>
   );
 };
