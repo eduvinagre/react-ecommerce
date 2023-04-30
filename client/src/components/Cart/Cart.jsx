9831056e6f93e33fbd4dfd14bf4e84a0e9efe9e1
eduvinagre <eduvinagre@gmail.com>
2023-04-30 17:56:38 -0300

    cart initial commit

---
+++ client/src/components/Cart/Cart.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Cart.scss";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+
+const Cart = () => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto-compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "T-shirt gráfica de manga comprida",
+      desc: "Blusa de manga comprida",
+      isNew: true,
+      oldPrice: 99.99,
+      price: 49.99,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Casaco",
+      desc: "Casco em material sintético",
+      isNew: true,
+      oldPrice: 129.99,
+      price: 89.99,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Saia",
+      desc: "Saia colorida",
+      isNew: false,
+      oldPrice: 149.99,
+      price: 99.99,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrob&w=1600",
+      title: "Chapéu",
+      desc: "Chapéu sertanejo",
+      isNew: false,
+      oldPrice: 79.99,
+      price: 29.99,
+    },
+  ];
+
+  return (
+    <div className="cart">
+      <h1>Produtos no carrinho</h1>
+      {data?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc?.substring(0, 100)}</p>
+            <div className="price">1 x {item.price}</div>
+          </div>
+          <DeleteOutlinedIcon className="delete" />
+        </div>
+      ))}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>R$ 123.45</span>
+      </div>
+      <button>PROSSEGUIR COM CHECKOUT</button>
+      <span className="reset">Limpar Carrinho</span>
+    </div>
+  );
+};
+
+export default Cart;
--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.scss";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 
 const Product = () => {
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  const images = [
+    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  ];
+
   return (
     <div className="product">
-      <div className="div"></div>
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Título</h1>
+        <span className="price">R$ 199.00</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ad
+          expedita nulla optio tenetur rerum rem consectetur dolore. Voluptas
+          consectetur veniam dicta quisquam delectus labore minima molestias,
+          alias reprehenderit assumenda!
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon />
+          ADICIONAR AO CARRINHO
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon />
+            LISTA DE DESEJOS
+          </div>
+          <div className="item">
+            <BalanceIcon />
+            COMPARAR
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendedor: Polo</span>
+          <span>Tipo de Produto: T-Shirt</span>
+          <span>Tag: T-Shirt, Feminino, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIÇÃO</span>
+          <hr />
+          <span>INFORMAÇÃO ADICIONAL</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
     </div>
   );
 };
