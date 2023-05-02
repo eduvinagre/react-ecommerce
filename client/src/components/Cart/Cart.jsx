c41f8fac6ec5d1a8b5479bf3a2f772d5c3ef2b8f
eduvinagre <eduvinagre@gmail.com>
2023-05-02 16:16:14 -0300

    redux perisis first commit

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -9,27 +9,59 @@ import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
 
+  const totalPrice = () => {
+    let total = 0;
+    products.forEach((item) => {
+      total += item.quantity * item.price;
+    });
+    return total.toFixed(2);
+  };
+
+  const stripePromise = loadStripe(
+    "pk_test_eOTMlr8usx1ctymXqrik0ls700lQCsX2UB"
+  );
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="cart">
-      <h1>Produtos no carrinho</h1>
+      <h1>Produtos no Carrinho</h1>
       {products?.map((item) => (
         <div className="item" key={item.id}>
-          <img src={item.img} alt="" />
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
           <div className="details">
             <h1>{item.title}</h1>
             <p>{item.desc?.substring(0, 100)}</p>
-            <div className="price">1 x {item.price}</div>
+            <div className="price">
+              {item.quantity} x ${item.price}
+            </div>
           </div>
-          <DeleteOutlinedIcon className="delete" />
+          <DeleteOutlinedIcon
+            className="delete"
+            onClick={() => dispatch(removeItem(item.id))}
+          />
         </div>
       ))}
       <div className="total">
         <span>SUBTOTAL</span>
-        <span>R$ 123.45</span>
+        <span>R${totalPrice()}</span>
       </div>
-      <button>PROSSEGUIR PARA CHECKOUT</button>
-      <span className="reset">Limpar Carrinho</span>
+      <button onClick={handlePayment}>PAGAR</button>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Limpar Carrinho
+      </span>
     </div>
   );
 };
